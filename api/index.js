85f4a6f541c99d4af86872eacbaf5d93e535e264
Robin Gouda <robin2contact@gmail.com>
2024-02-22 02:38:06 +0530

    created api route

--- api/index.js
+++ api/index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
+import userRouter from "./routes/user.route.js";
 
 const app = express();
 mongoose
   .connect(process.env.MONGO_DB)
   .then(() => {
-    console.log(`connected to DB!`);
+    console.log(`connected to DB! `);
   })
   .catch((err) => {
     console.log(err);
@@ -14,8 +15,6 @@ mongoose
 
 const port = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("I am the server");
-});
-
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+app.use("/api/user", userRouter);
