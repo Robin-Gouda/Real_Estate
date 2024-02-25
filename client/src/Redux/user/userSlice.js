70294419ce462bc0b4bc212584d408f8958ea5da
Robin Gouda <robin2contact@gmail.com>
2024-02-25 20:23:20 +0530

    slice design

--- client/src/Redux/user/userSlice.js
+++ client/src/Redux/user/userSlice.js
@@ -6,24 +6,24 @@ const initialState = {
   loading: false,
 };
 
-const userSlice = createSlice({
-  name: "user",
-  initialState,
-  reducers: {
-    signInStart: (state) => {
-      state.loading = true;
-    },
-    signInSuccess: (state, action) => {
-      state.currentUser = action.payload;
-      state.loading = false;
-      state.error = null;
-    },
-    signInFailure: (state, action) => {
-      state.error = action.payload;
-      state.loading = false;
-    },
-  },
-});
+// const userSlice = createSlice({
+//   name: "user",
+//   initialState,
+//   reducers: {
+//     signInStart: (state) => {
+//       state.loading = true;
+//     },
+//     signInSuccess: (state, action) => {
+//       state.currentUser = action.payload;
+//       state.loading = false;
+//       state.error = null;
+//     },
+//     signInFailure: (state, action) => {
+//       state.error = action.payload;
+//       state.loading = false;
+//     },
+//   },
+// });
 
 export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
 export default userSlice.reducer;
