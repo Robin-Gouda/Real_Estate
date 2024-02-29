266504e904f13ed74f85cb1c54b5223628b95e5a
Robin Gouda <robin2contact@gmail.com>
2024-02-29 16:47:16 +0530

    create and OAuth

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -5,6 +5,11 @@ const userSchema = new mongoose.Schema(
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    avatar: {
+      type: String,
+      default:
+        "https://isobarscience-1bfd8.kxcdn.com/wp-content/uploads/2020/09/default-profile-picture1.jpg",
+    },
   },
   { timestamps: true }
 );
